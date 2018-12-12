395cd72d9250f20f21c2c00717b6aaa159292bfd
weikkd <weikkd@rose-hulman.edu>
2018-12-11 20:25:34 -0500

    Done.

--- src/problem1.py
+++ src/problem1.py
@@ -3,13 +3,13 @@ Exam 1, problem 1.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Kaitlin Weik.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 # -----------------------------------------------------------------------------
-# TODO: 2. Right-click on the  src  folder and
+# DONE: 2. Right-click on the  src  folder and
 #              Mark Directory as ... Sources Root,
 #          if you have not already done so.
 # -----------------------------------------------------------------------------
@@ -104,6 +104,9 @@ def problem1(square, thickness, window):
     # TODO: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    circle = rg.Circle
+    circle.fill_color = square.fill_color
 
 
 # -----------------------------------------------------------------------------
--- src/problem2.py
+++ src/problem2.py
@@ -3,19 +3,49 @@ Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Kaitlin Weik.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
     test_factor_sum()
 
+    # Following code block (sum_of_digits) is taken from previous coding projects
+
+def sum_of_digits(number):
+    """
+    What comes in:  An integer.
+    What goes out:  Returns the sum of the digits in the given integer.
+    Side effects:   None.
+    Example:
+      If the integer is 83135,
+      this function returns (8 + 3 + 1 + 3 + 5), which is 20.
+    """
+    # -------------------------------------------------------------------------
+    # Students:
+    #   Do NOT touch the above  sum_of_digits function - it has no _TODO_.
+    #   Do NOT copy code from this function.
+    #
+    # Instead, ** CALL ** this function as needed in the problems below.
+    # -------------------------------------------------------------------------
+    if number < 0:
+        number = -number
+
+    digit_sum = 0
+    while True:
+        if number == 0:
+            break
+        digit_sum = digit_sum + (number % 10)
+        number = number // 10
+
+    return digit_sum
+
 
 def test_factor_sum():
     """ Tests the   factor_sum   function. """
     ###########################################################################
-    #  TODO: 2. Implement this TEST function, as follows:
+    #  DONE: 2. Implement this TEST function, as follows:
     #
     #    1. Read the  doc-string of the   factor_sum   function defined below.
     #
@@ -45,6 +75,31 @@ def test_factor_sum():
     # WRITE YOUR TESTS BELOW HERE:
     ###########################################################################
 
+    print('Test #1:')
+    print('Expected Value = 18')
+    actual = factor_sum(187369)
+    print('Actual Value = ', actual)
+
+    print('Test #2:')
+    print('Expected Value = 15')
+    actual = factor_sum(69)
+    print('Actual Value = ', actual)
+
+    print('Test #3:')
+    print('Expected Value = 1')
+    actual = factor_sum(1)
+    print('Actual Value = ', actual)
+
+    print('Test #4:')
+    print('Expected Value = 4')
+    actual = factor_sum(25)
+    print('Actual Value = ', actual)
+
+    print('Test #5:')
+    print('Expected Value = 11')
+    actual = factor_sum(28)
+    print('Actual Value = ', actual)
+
 
 def factor_sum(n):
     """
@@ -70,6 +125,14 @@ def factor_sum(n):
 
        *** ASK FOR AN EXPLANATION IF YOU DO NOT UNDERSTAND THE ABOVE. ***
     """
+
+    value = 0
+    for k in range(1, n + 1):
+        if n % k == 0:
+            value = value + k
+    sum_of = sum_of_digits(value)
+    return sum_of
+
     ###########################################################################
     #  This function is PURPOSELY implemented INCORRECTLY (it just returns 0).
     #  DO NOT IMPLEMENT  factor_sum.  Just leave it as it is (returning 0).
@@ -78,3 +141,6 @@ def factor_sum(n):
     ###########################################################################
     # DO NOT modify the above line of code!
     ###########################################################################
+
+
+main()
